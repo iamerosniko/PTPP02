aa302cb32729a9da7ad2ecead2a3086b4d6df560
Eros Niko Cas Alvarez <iamerosniko@yahoo.com>
2018-05-23 14:26:57 -0400

    up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module'
 
+
+import { TopNavComponent } from './views/top-nav/top-nav.component';
 
 import { AppComponent } from './app.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,TopNavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
