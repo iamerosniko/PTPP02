fa0a206125cb106b8e58fde62acfaaae101762e0
Ambrotix <ambrociojoeniel@gmail.com>
2018-05-30 16:56:14 -0400

    Search and Reports
    Search Component - partial
    Reports Component

--- src/app/views/reports/reports.module.ts
+++ src/app/views/reports/reports.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
 import { CommonModule } from '@angular/common';
 import { ReportsComponent } from './reports.component';
 
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,RouterModule
   ],
   declarations: [ReportsComponent]
 })
