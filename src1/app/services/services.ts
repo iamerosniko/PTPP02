60042d30d8e6ae01df5e575233f773cc84d382df
Eros Niko Cas Alvarez <iamerosniko@yahoo.com>
2018-09-14 09:01:22 -0400

    up

--- src1/app/services/services.ts
+++
@@ -1,7 +0,0 @@
-export * from './department.service'
-export * from './category.service'
-export * from './centricscore.service'
-export * from './clientapi.service'
-export * from './clientapi.settings'
-export * from './contact.service'
-export * from './mapcode.service'
--- src1/app/views/search/search.ts
+++
@@ -1,3 +0,0 @@
-export * from './search.component'
-export * from './search-details/search-details.component'
-export * from './search-tasks/search-tasks.component'
