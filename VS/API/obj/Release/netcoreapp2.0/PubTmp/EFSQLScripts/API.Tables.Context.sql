14634285da92855b381b87995152e738f27e38de
Eros Niko Cas Alvarez <iamerosniko@yahoo.com>
2018-09-21 14:40:09 -0400

    up

--- VS/API/obj/Release/netcoreapp2.0/PubTmp/EFSQLScripts/API.Tables.Context.sql
+++ VS/API/obj/Release/netcoreapp2.0/PubTmp/EFSQLScripts/API.Tables.Context.sql
@@ -9,22 +9,24 @@ END;
 
 GO
 
-IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180530191529_InitialCreate')
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180917125726_initialCreate')
 BEGIN
     CREATE TABLE [PP_Categories] (
-        [CategoryID] int NOT NULL IDENTITY,
+        [CategoryID] uniqueidentifier NOT NULL,
         [Category] nvarchar(max) NULL,
+        [GroupName] nvarchar(max) NULL,
         CONSTRAINT [PK_PP_Categories] PRIMARY KEY ([CategoryID])
     );
 END;
 
 GO
 
-IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180530191529_InitialCreate')
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180917125726_initialCreate')
 BEGIN
     CREATE TABLE [PP_CentricScores] (
-        [CentricScoreID] int NOT NULL IDENTITY,
-        [Score] int NOT NULL,
+        [CentricScoreID] uniqueidentifier NOT NULL,
+        [GroupName] nvarchar(max) NULL,
+        [Score] uniqueidentifier NOT NULL,
         [ScoreDesc] nvarchar(max) NULL,
         CONSTRAINT [PK_PP_CentricScores] PRIMARY KEY ([CentricScoreID])
     );
@@ -32,24 +34,26 @@ END;
 
 GO
 
-IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180530191529_InitialCreate')
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180917125726_initialCreate')
 BEGIN
     CREATE TABLE [PP_Comments] (
-        [MainID] int NOT NULL IDENTITY,
+        [MainID] uniqueidentifier NOT NULL,
         [Comment] nvarchar(max) NULL,
         [DateCreated] datetime2 NOT NULL,
         [FullName] nvarchar(max) NULL,
+        [GroupName] nvarchar(max) NULL,
         CONSTRAINT [PK_PP_Comments] PRIMARY KEY ([MainID])
     );
 END;
 
 GO
 
-IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180530191529_InitialCreate')
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180917125726_initialCreate')
 BEGIN
     CREATE TABLE [PP_Contacts] (
-        [ContactID] int NOT NULL IDENTITY,
+        [ContactID] uniqueidentifier NOT NULL,
         [ContactName] nvarchar(max) NULL,
+        [GroupName] nvarchar(max) NULL,
         [LastName] nvarchar(max) NULL,
         CONSTRAINT [PK_PP_Contacts] PRIMARY KEY ([ContactID])
     );
@@ -57,45 +61,23 @@ END;
 
 GO
 
-IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180530191529_InitialCreate')
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180917125726_initialCreate')
 BEGIN
     CREATE TABLE [PP_Departments] (
-        [DepartmentID] int NOT NULL IDENTITY,
+        [DepartmentID] uniqueidentifier NOT NULL,
         [Department] nvarchar(max) NULL,
+        [GroupName] nvarchar(max) NULL,
         CONSTRAINT [PK_PP_Departments] PRIMARY KEY ([DepartmentID])
     );
 END;
 
 GO
 
-IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180530191529_InitialCreate')
-BEGIN
-    CREATE TABLE [PP_Main] (
-        [MainID] int NOT NULL IDENTITY,
-        [CategoryID] int NOT NULL,
-        [CentricScoreID] int NOT NULL,
-        [DateAdded] datetime2 NOT NULL,
-        [DateEnded] datetime2 NOT NULL,
-        [DatePrevious] datetime2 NOT NULL,
-        [DateStarted] datetime2 NOT NULL,
-        [DepartmentID] int NOT NULL,
-        [MapCode] nvarchar(max) NULL,
-        [Overview] nvarchar(max) NULL,
-        [Status] nvarchar(max) NULL,
-        [TargetDate] datetime2 NOT NULL,
-        [TaskCompleted] int NOT NULL,
-        [TaskInProgress] int NOT NULL,
-        [TaskUpcoming] int NOT NULL,
-        CONSTRAINT [PK_PP_Main] PRIMARY KEY ([MainID])
-    );
-END;
-
-GO
-
-IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180530191529_InitialCreate')
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180917125726_initialCreate')
 BEGIN
     CREATE TABLE [PP_MapCodes] (
-        [MapCodeID] int NOT NULL IDENTITY,
+        [MapCodeID] uniqueidentifier NOT NULL,
+        [GroupName] nvarchar(max) NULL,
         [MapCode] nvarchar(max) NULL,
         CONSTRAINT [PK_PP_MapCodes] PRIMARY KEY ([MapCodeID])
     );
@@ -103,11 +85,37 @@ END;
 
 GO
 
-IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180530191529_InitialCreate')
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180917125726_initialCreate')
+BEGIN
+    CREATE TABLE [PP_Project] (
+        [ProjectID] uniqueidentifier NOT NULL,
+        [CustomerCentricScore] uniqueidentifier NOT NULL,
+        [CustomerMapCode] nvarchar(max) NULL,
+        [DateAdded] datetime2 NOT NULL,
+        [DepartmentID] uniqueidentifier NOT NULL,
+        [EndDate] datetime2 NOT NULL,
+        [EndDateChanged] bit NOT NULL,
+        [ProjectCategory] uniqueidentifier NOT NULL,
+        [ProjectManager] uniqueidentifier NOT NULL,
+        [ProjectNumber] nvarchar(max) NULL,
+        [ProjectOverview] nvarchar(max) NULL,
+        [ProjectSponsor] uniqueidentifier NOT NULL,
+        [ProjectStakeHolder] uniqueidentifier NOT NULL,
+        [ProjectTargetDate] datetime2 NOT NULL,
+        [StartDate] datetime2 NOT NULL,
+        [Status] uniqueidentifier NOT NULL,
+        CONSTRAINT [PK_PP_Project] PRIMARY KEY ([ProjectID])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180917125726_initialCreate')
 BEGIN
     CREATE TABLE [PP_ProjectContacts] (
-        [MainID] int NOT NULL IDENTITY,
+        [MainID] uniqueidentifier NOT NULL,
         [Contacts] nvarchar(max) NULL,
+        [GroupName] nvarchar(max) NULL,
         [Type] nvarchar(max) NULL,
         CONSTRAINT [PK_PP_ProjectContacts] PRIMARY KEY ([MainID])
     );
@@ -115,74 +123,31 @@ END;
 
 GO
 
-IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180530191529_InitialCreate')
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180917125726_initialCreate')
 BEGIN
     INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
-    VALUES (N'20180530191529_InitialCreate', N'2.0.1-rtm-125');
+    VALUES (N'20180917125726_initialCreate', N'2.0.1-rtm-125');
 END;
 
 GO
 
-IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180604111245_m001')
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180917135017_m001')
 BEGIN
-    ALTER TABLE [PP_ProjectContacts] ADD [GroupName] nvarchar(max) NULL;
+    DECLARE @var0 sysname;
+    SELECT @var0 = [d].[name]
+    FROM [sys].[default_constraints] [d]
+    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+    WHERE ([d].[parent_object_id] = OBJECT_ID(N'PP_CentricScores') AND [c].[name] = N'Score');
+    IF @var0 IS NOT NULL EXEC(N'ALTER TABLE [PP_CentricScores] DROP CONSTRAINT [' + @var0 + '];');
+    ALTER TABLE [PP_CentricScores] ALTER COLUMN [Score] nvarchar(max) NULL;
 END;
 
 GO
 
-IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180604111245_m001')
-BEGIN
-    ALTER TABLE [PP_MapCodes] ADD [GroupName] nvarchar(max) NULL;
-END;
-
-GO
-
-IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180604111245_m001')
-BEGIN
-    ALTER TABLE [PP_Main] ADD [GroupName] nvarchar(max) NULL;
-END;
-
-GO
-
-IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180604111245_m001')
-BEGIN
-    ALTER TABLE [PP_Departments] ADD [GroupName] nvarchar(max) NULL;
-END;
-
-GO
-
-IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180604111245_m001')
-BEGIN
-    ALTER TABLE [PP_Contacts] ADD [GroupName] nvarchar(max) NULL;
-END;
-
-GO
-
-IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180604111245_m001')
-BEGIN
-    ALTER TABLE [PP_Comments] ADD [GroupName] nvarchar(max) NULL;
-END;
-
-GO
-
-IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180604111245_m001')
-BEGIN
-    ALTER TABLE [PP_CentricScores] ADD [GroupName] nvarchar(max) NULL;
-END;
-
-GO
-
-IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180604111245_m001')
-BEGIN
-    ALTER TABLE [PP_Categories] ADD [GroupName] nvarchar(max) NULL;
-END;
-
-GO
-
-IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180604111245_m001')
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180917135017_m001')
 BEGIN
     INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
-    VALUES (N'20180604111245_m001', N'2.0.1-rtm-125');
+    VALUES (N'20180917135017_m001', N'2.0.1-rtm-125');
 END;
 
 GO
