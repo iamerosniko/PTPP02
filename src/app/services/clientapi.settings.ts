200ce42ea503eb05d2a337082186e394a17c8e22
Eros Niko Cas Alvarez <iamerosniko@yahoo.com>
2018-11-29 14:41:37 -0500

    up'

--- src/app/services/clientapi.settings.ts
+++ src/app/services/clientapi.settings.ts
@@ -1,7 +1,8 @@
 export class ClientApiSettings {
 
     //change the url according to client's url
-    private static Client_URL = "https://localhost:44370/api/"
+    // private static Client_URL = "https://localhost:44370/api/"
+    private static Client_URL = "api/"
     //change the url according for business workflow urls
     private static API_URL = "https://pplapid.azurewebsites.net/api/"
     // private static API_URL = "http://localhost:52013/api/"
